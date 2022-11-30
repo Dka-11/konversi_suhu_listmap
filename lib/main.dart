55a131f521c1087c0663c9a17ca05353ba5a164a
Dka-11 <adikaahmad05@gmail.com>
2022-11-30 23:27:09 +0700

    Convert to Statefull Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,16 @@ import 'package:konversi_suhu/widgets/button_suhu.dart';
 import 'package:konversi_suhu/widgets/form_suhu.dart';
 import 'package:konversi_suhu/widgets/text_suhu.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
