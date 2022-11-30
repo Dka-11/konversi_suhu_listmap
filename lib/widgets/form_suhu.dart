5b9c3f8495cbf25f053def5f42ccb3e0230c88eb
Dka-11 <adikaahmad05@gmail.com>
2022-12-01 00:19:44 +0700

    Create TextEdtingController

--- lib/widgets/form_suhu.dart
+++ lib/widgets/form_suhu.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class FormSuhu extends StatelessWidget {
-  const FormSuhu({Key? key}) : super(key: key);
+class FormSuhu extends StatefulWidget {
+  const FormSuhu({super.key});
+
+  @override
+  State<FormSuhu> createState() => _FormSuhuState();
+}
+
+class _FormSuhuState extends State<FormSuhu> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: myController,
       decoration: const InputDecoration(
         hintText: 'Masukkan Suhu Dalam Celcius',
         labelText: "Masukkan Suhu Dalam Celcius",
