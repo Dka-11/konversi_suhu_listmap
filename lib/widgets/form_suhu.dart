6f84afe1f477b4b3f96f1761b25b7a2824730d0e
Dka-11 <adikaahmad05@gmail.com>
2022-12-01 20:33:38 +0700

    Revert Stateless

--- lib/widgets/form_suhu.dart
+++ lib/widgets/form_suhu.dart
@@ -1,26 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class FormSuhu extends StatefulWidget {
+class FormSuhu extends StatelessWidget {
   const FormSuhu({super.key});
-
-  @override
-  State<FormSuhu> createState() => _FormSuhuState();
-}
-
-class _FormSuhuState extends State<FormSuhu> {
-  final myController = TextEditingController();
-
-  @override
-  void dispose() {
-    myController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: myController,
       decoration: const InputDecoration(
         hintText: 'Masukkan Suhu Dalam Celcius',
         labelText: "Masukkan Suhu Dalam Celcius",
